666ada343ca289a5a573823edcfe7e0e022c7625
junesyigatsu <june_aqj69@rocketmail.com>
2013-01-28 09:22:44 +0700

    config.php

--- document/application/config/config.php
+++ document/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/document/';
+$config['base_url']	= 'http://localhost/documentmanagement/documentmanagement/document';
 
 /*
 |--------------------------------------------------------------------------
